99bfd86c3d7b10d0f20f0ed7592f200c154c49f2
pexcn <i@pexcn.me>
2018-04-08 13:20:20 +0800

    Add dnsmasq rules full version

--- generate.sh
+++ generate.sh
@@ -12,6 +12,7 @@ function prepare_env() {
   popd
 }
 
+# chnroute
 function build_chnroute() {
   # ipv4 chnroute
   mkdir -p build/chnroute
@@ -29,14 +30,22 @@ function build_chnroute() {
   popd
 }
 
+# dnsmasq rules
 function build_dnsmasq_rules() {
-  # dnsmasq rules
   mkdir -p build/dnsmasq
   pushd build/dnsmasq
+
+  # normal
   curl -kL https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > easylistchina.conf.tmp
   curl -kL https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > abp-fx.conf.tmp
   curl -kL "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" | grep -E '^127.0.0.1' | awk '{printf "address=/%s/127.0.0.1\n",$2}' > yoyo.conf.tmp
+
+  # full
+  curl -kL https://hosts-file.net/ad_servers.txt | grep -E '^127.0.0.1' | awk '{printf "address=/%s/127.0.0.1\n",$2}' > adaway.conf.tmp
+
   cat easylistchina.conf.tmp abp-fx.conf.tmp yoyo.conf.tmp | sort | uniq > adblock.conf
+  cat easylistchina.conf.tmp abp-fx.conf.tmp yoyo.conf.tmp adaway.conf.tmp | sort | uniq > adblock_full.conf
+
   popd
 }
 
@@ -47,6 +56,7 @@ function clean_up() {
   rm build/dnsmasq/easylistchina.conf.tmp
   rm build/dnsmasq/abp-fx.conf.tmp
   rm build/dnsmasq/yoyo.conf.tmp
+  rm build/dnsmasq/adaway.conf.tmp
 }
 
 function dist_release() {
