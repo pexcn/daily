ed0ee80f9605754b5964ec7dfbced0e78aed8eba
pexcn <i@pexcn.me>
2018-04-08 12:44:28 +0800

    Fix dnsmasq rules generate logic

--- generate.sh
+++ generate.sh
@@ -29,26 +29,21 @@ function build_chnroute() {
   popd
 }
 
-# TODO: write it pretty
 function build_dnsmasq_rules() {
   # dnsmasq rules
   mkdir -p build/dnsmasq
   pushd build/dnsmasq
-  echo -e "#\n# easylistchina+easylist\n#" >> adblock.conf.tmp
-  curl -kL https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' | sort | uniq >> adblock.conf.tmp
-  echo -e "\n\n" >> adblock.conf.tmp
-  echo -e "#\n# ABP-FX\n#" >> adblock.conf.tmp
-  curl -kL https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' | sort | uniq >> adblock.conf.tmp
-  echo -e "\n\n" >> adblock.conf.tmp
-  echo -e "#\n# custom rules\n#" >> adblock.conf.tmp
-  curl -kL https://pexcn.github.io/dnsmasq-rules/rules.conf >> adblock.conf.tmp
-  mv adblock.conf.tmp adblock.conf
+  curl -kL https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > easylistchina.conf.tmp
+  curl -kL https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > abp-fx.conf.tmp
+  cat easylistchina.conf.tmp abp-fx.conf.tmp | sort | uniq > adblock.conf
   popd
 }
 
 function clean_up() {
   rm build/chnroute/chnroute.txt.tmp
   rm build/chnroute/chnroute_ipip.txt.tmp
+  rm build/dnsmasq/easylistchina.conf.tmp
+  rm build/dnsmasq/abp-fx.conf.tmp
 }
 
 function dist_release() {
