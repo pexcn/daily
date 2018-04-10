e0789499a89f44af927acd20893421aec33087ab
pexcn <i@pexcn.me>
2018-04-10 16:02:09 +0800

    dnsmasq-rules: add update time comments

--- generate.sh
+++ generate.sh
@@ -32,17 +32,15 @@ function gen_dnsmasq_rules() {
   mkdir -p gen/dnsmasq
   pushd gen/dnsmasq
 
-  # normal
   curl -kL 'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt' | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > easylistchina.conf.tmp
   curl -kL 'https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt' | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > abp-fx.conf.tmp
   curl -kL 'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext' | grep -E '^127.0.0.1' | awk '{printf "address=/%s/127.0.0.1\n",$2}' > yoyo.conf.tmp
-
-  # full
   curl -kL 'https://hosts-file.net/ad_servers.txt' | grep -E '^127.0.0.1' | awk '{printf "address=/%s/127.0.0.1\n",$2}' > adaway.conf.tmp
 
   cat easylistchina.conf.tmp abp-fx.conf.tmp yoyo.conf.tmp | sort | uniq > adblock.conf
   cat easylistchina.conf.tmp abp-fx.conf.tmp yoyo.conf.tmp adaway.conf.tmp | sort | uniq > adblock_full.conf
 
+  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" adblock.conf adblock_full.conf
   popd
 }
 
