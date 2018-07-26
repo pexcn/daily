10dfbdf21199cb3ee89c0846617ecd9263f89b2d
pexcn <i@pexcn.me>
2018-07-26 10:59:55 +0800

    refine chnroute script

--- scripts/chnroute/chnroute.sh
+++ scripts/chnroute/chnroute.sh
@@ -2,41 +2,53 @@
 
 TMP_DIR=`mktemp -d /tmp/chnroute.XXXXXX`
 DIST_DIR='dist/chnroute'
+DIST_FILE_IPV4='chnroute.txt'
+DIST_FILE_IPV6='chnroute_v6.txt'
 
 APNIC_URL='https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
 IPIP_URL='https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt'
 
+APNIC_LIST='chnroute_apnic.txt'
+IPIP_LIST='chnroute_ipip.txt'
+
 function fetch_data() {
   pushd $TMP_DIR
-  curl -kLs $APNIC_URL > chnroute_apnic.txt
-  curl -kLs $IPIP_URL > chnroute_ipip.txt
+  curl -kLs $APNIC_URL > $APNIC_LIST
+  curl -kLs $IPIP_URL > $IPIP_LIST
   popd
 }
 
 function gen_ipv4_chnroute() {
   pushd $TMP_DIR
 
-  cat chnroute_apnic.txt | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chnroute_apnic.tmp
-  cat chnroute_ipip.txt > chnroute_ipip.tmp
-  cat chnroute_apnic.tmp chnroute_ipip.tmp | cidrmerge > chnroute.tmp
+  local chnroute_apnic_content='chnroute_apnic_content_v4.tmp'
+  local chnroute_ipip_content='chnroute_ipip_content_v4.tmp'
+  local chnroute_content='chnroute_content_v4.tmp'
 
-  mv chnroute.tmp chnroute.txt
+  cat $APNIC_LIST | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > $chnroute_apnic_content
+  cat $IPIP_LIST > $chnroute_ipip_content
+  #cat $chnroute_apnic_content $chnroute_ipip_content | cidrmerge > $chnroute_content
+  cat $chnroute_apnic_content $chnroute_ipip_content > $chnroute_content
+
+  mv $chnroute_content $DIST_FILE_IPV4
   popd
 }
 
 function gen_ipv6_chnroute() {
   pushd $TMP_DIR
 
-  cat chnroute_apnic.txt | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chnroute_v6.tmp
+  local chnroute_content='chnroute_content_v6.tmp'
 
-  mv chnroute_v6.tmp chnroute_v6.txt
+  cat $APNIC_LIST | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > $chnroute_content
+
+  mv $chnroute_content $DIST_FILE_IPV6
   popd
 }
 
 function dist_release() {
   mkdir -p $DIST_DIR
-  mv $TMP_DIR/chnroute.txt $DIST_DIR
-  mv $TMP_DIR/chnroute_v6.txt $DIST_DIR
+  mv $TMP_DIR/$DIST_FILE_IPV4 $DIST_DIR
+  mv $TMP_DIR/$DIST_FILE_IPV6 $DIST_DIR
 }
 
 function clean_up() {
