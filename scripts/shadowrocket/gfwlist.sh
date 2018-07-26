4b1723c11353c8e7e6d4465655af694048739ef8
pexcn <i@pexcn.me>
2018-07-26 15:26:15 +0800

    refine shadowrocket gfwlist script

--- scripts/shadowrocket/gfwlist.sh
+++ scripts/shadowrocket/gfwlist.sh
@@ -2,53 +2,74 @@
 
 TMP_DIR=`mktemp -d /tmp/shadowrocket.XXXXXX`
 DIST_DIR='dist/shadowrocket'
+DIST_FILE='gfwlist.conf'
 
 GFWLIST_URL='https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt'
+GFWLIST='gfwlist.txt'
+
+GFWLIST_CONTENT='gfwlist_content.tmp'
 
 function fetch_data() {
-  cp template/shadowrocket/gfwlist.conf $TMP_DIR
+  local config_template='template/shadowrocket/gfwlist.conf'
+  local extra_template='template/gfwlist_extra_domains.txt'
 
-  pushd $TMP_DIR
-  curl -kLs $GFWLIST_URL | base64 -d > gfwlist.txt
+  cp ${config_template} ${TMP_DIR}
+  cp ${extra_template} ${TMP_DIR}
+
+  pushd ${TMP_DIR}
+  curl -kLs ${GFWLIST_URL} | base64 -d > ${GFWLIST}
   popd
 }
 
-function gen_gfwlist_pac() {
-  pushd $TMP_DIR
+function extract_domains() {
+  pushd ${TMP_DIR}
+
+  local gfwlist_domains='gfwlist_domains.txt'
+  local gfwlist_extra_domains='gfwlist_extra_domains.txt'
 
   # patterns from @cokebar/gfwlist2dnsmasq
-  IGNORE_PATTERN='^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
-  HEAD_FILTER_PATTERN='s#^(\|\|?)?(https?://)?##g'
-  TAIL_FILTER_PATTERN='s#/.*$|%2F.*$##g'
-  DOMAIN_PATTERN='([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)'
-  WILDCARD_PATTERN='s#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*)?#\4#g'
+  local ignore_pattern='^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+  local head_filter_pattern='s#^(\|\|?)?(https?://)?##g'
+  local tail_filter_pattern='s#/.*$|%2F.*$##g'
+  local domain_pattern='([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)'
+  local wildcard_pattern='s#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*)?#\4#g'
 
-  grep -vE $IGNORE_PATTERN gfwlist.txt | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | grep -E $DOMAIN_PATTERN | sed -r $WILDCARD_PATTERN > gfwlist.tmp
-  printf 'google.com\ngoogle.ad\ngoogle.ae\ngoogle.com.af\ngoogle.com.ag\ngoogle.com.ai\ngoogle.al\ngoogle.am\ngoogle.co.ao\ngoogle.com.ar\ngoogle.as\ngoogle.at\ngoogle.com.au\ngoogle.az\ngoogle.ba\ngoogle.com.bd\ngoogle.be\ngoogle.bf\ngoogle.bg\ngoogle.com.bh\ngoogle.bi\ngoogle.bj\ngoogle.com.bn\ngoogle.com.bo\ngoogle.com.br\ngoogle.bs\ngoogle.bt\ngoogle.co.bw\ngoogle.by\ngoogle.com.bz\ngoogle.ca\ngoogle.cd\ngoogle.cf\ngoogle.cg\ngoogle.ch\ngoogle.ci\ngoogle.co.ck\ngoogle.cl\ngoogle.cm\ngoogle.cn\ngoogle.com.co\ngoogle.co.cr\ngoogle.com.cu\ngoogle.cv\ngoogle.com.cy\ngoogle.cz\ngoogle.de\ngoogle.dj\ngoogle.dk\ngoogle.dm\ngoogle.com.do\ngoogle.dz\ngoogle.com.ec\ngoogle.ee\ngoogle.com.eg\ngoogle.es\ngoogle.com.et\ngoogle.fi\ngoogle.com.fj\ngoogle.fm\ngoogle.fr\ngoogle.ga\ngoogle.ge\ngoogle.gg\ngoogle.com.gh\ngoogle.com.gi\ngoogle.gl\ngoogle.gm\ngoogle.gp\ngoogle.gr\ngoogle.com.gt\ngoogle.gy\ngoogle.com.hk\ngoogle.hn\ngoogle.hr\ngoogle.ht\ngoogle.hu\ngoogle.co.id\ngoogle.ie\ngoogle.co.il\ngoogle.im\ngoogle.co.in\ngoogle.iq\ngoogle.is\ngoogle.it\ngoogle.je\ngoogle.com.jm\ngoogle.jo\ngoogle.co.jp\ngoogle.co.ke\ngoogle.com.kh\ngoogle.ki\ngoogle.kg\ngoogle.co.kr\ngoogle.com.kw\ngoogle.kz\ngoogle.la\ngoogle.com.lb\ngoogle.li\ngoogle.lk\ngoogle.co.ls\ngoogle.lt\ngoogle.lu\ngoogle.lv\ngoogle.com.ly\ngoogle.co.ma\ngoogle.md\ngoogle.me\ngoogle.mg\ngoogle.mk\ngoogle.ml\ngoogle.com.mm\ngoogle.mn\ngoogle.ms\ngoogle.com.mt\ngoogle.mu\ngoogle.mv\ngoogle.mw\ngoogle.com.mx\ngoogle.com.my\ngoogle.co.mz\ngoogle.com.na\ngoogle.com.nf\ngoogle.com.ng\ngoogle.com.ni\ngoogle.ne\ngoogle.nl\ngoogle.no\ngoogle.com.np\ngoogle.nr\ngoogle.nu\ngoogle.co.nz\ngoogle.com.om\ngoogle.com.pa\ngoogle.com.pe\ngoogle.com.pg\ngoogle.com.ph\ngoogle.com.pk\ngoogle.pl\ngoogle.pn\ngoogle.com.pr\ngoogle.ps\ngoogle.pt\ngoogle.com.py\ngoogle.com.qa\ngoogle.ro\ngoogle.ru\ngoogle.rw\ngoogle.com.sa\ngoogle.com.sb\ngoogle.sc\ngoogle.se\ngoogle.com.sg\ngoogle.sh\ngoogle.si\ngoogle.sk\ngoogle.com.sl\ngoogle.sn\ngoogle.so\ngoogle.sm\ngoogle.sr\ngoogle.st\ngoogle.com.sv\ngoogle.td\ngoogle.tg\ngoogle.co.th\ngoogle.com.tj\ngoogle.tk\ngoogle.tl\ngoogle.tm\ngoogle.tn\ngoogle.to\ngoogle.com.tr\ngoogle.tt\ngoogle.com.tw\ngoogle.co.tz\ngoogle.com.ua\ngoogle.co.ug\ngoogle.co.uk\ngoogle.com.uy\ngoogle.co.uz\ngoogle.com.vc\ngoogle.co.ve\ngoogle.vg\ngoogle.co.vi\ngoogle.com.vn\ngoogle.vu\ngoogle.ws\ngoogle.rs\ngoogle.co.za\ngoogle.co.zm\ngoogle.co.zw\ngoogle.cat\n' >> gfwlist.tmp
-  printf 'blogspot.ca\nblogspot.co.uk\nblogspot.com\nblogspot.com.ar\nblogspot.com.au\nblogspot.com.br\nblogspot.com.by\nblogspot.com.co\nblogspot.com.cy\nblogspot.com.ee\nblogspot.com.eg\nblogspot.com.es\nblogspot.com.mt\nblogspot.com.ng\nblogspot.com.tr\nblogspot.com.uy\nblogspot.de\nblogspot.gr\nblogspot.in\nblogspot.mx\nblogspot.ch\nblogspot.fr\nblogspot.ie\nblogspot.it\nblogspot.pt\nblogspot.ro\nblogspot.sg\nblogspot.be\nblogspot.no\nblogspot.se\nblogspot.jp\nblogspot.in\nblogspot.ae\nblogspot.al\nblogspot.am\nblogspot.ba\nblogspot.bg\nblogspot.ch\nblogspot.cl\nblogspot.cz\nblogspot.dk\nblogspot.fi\nblogspot.gr\nblogspot.hk\nblogspot.hr\nblogspot.hu\nblogspot.ie\nblogspot.is\nblogspot.kr\nblogspot.li\nblogspot.lt\nblogspot.lu\nblogspot.md\nblogspot.mk\nblogspot.my\nblogspot.nl\nblogspot.no\nblogspot.pe\nblogspot.qa\nblogspot.ro\nblogspot.ru\nblogspot.se\nblogspot.sg\nblogspot.si\nblogspot.sk\nblogspot.sn\nblogspot.tw\nblogspot.ug\nblogspot.cat\n' >> gfwlist.tmp
-  printf 'twimg.edgesuite.net\n' >> gfwlist.tmp
-  cat gfwlist.tmp | sort | uniq > gfwlist_domains.tmp
+  grep -vE ${ignore_pattern} ${GFWLIST} |
+      sed -r ${head_filter_pattern} |
+      sed -r ${tail_filter_pattern} |
+      grep -E ${domain_pattern} |
+      sed -r ${wildcard_pattern} > ${gfwlist_domains}
+  cat ${gfwlist_extra_domains} >> ${gfwlist_domains}
 
-  sed -i 's/^/DOMAIN-SUFFIX,/' gfwlist_domains.tmp
-  sed -i 's/$/,PROXY,force-remote-dns/' gfwlist_domains.tmp
+  cat ${gfwlist_domains} | sort | uniq > ${GFWLIST_CONTENT}
 
-  # replace content
-  sed -i 's/___GFWLIST_DOMAINS_PLACEHOLDER___/cat gfwlist_domains.tmp/e' gfwlist.conf
+  popd
+}
 
-  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" gfwlist.conf
+function gen_gfwlist_config() {
+  pushd ${TMP_DIR}
+
+  sed -i 's/^/DOMAIN-SUFFIX,/' ${GFWLIST_CONTENT}
+  sed -i 's/$/,PROXY,force-remote-dns/' ${GFWLIST_CONTENT}
+
+  sed -i "s/___GFWLIST_DOMAINS_PLACEHOLDER___/cat ${GFWLIST_CONTENT}/e" ${DIST_FILE}
+
+  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" ${DIST_FILE}
+
   popd
 }
 
 function dist_release() {
-  mkdir -p $DIST_DIR
-  mv $TMP_DIR/gfwlist.conf $DIST_DIR
+  mkdir -p ${DIST_DIR}
+  mv ${TMP_DIR}/${DIST_FILE} ${DIST_DIR}
 }
 
 function clean_up() {
-  rm -r $TMP_DIR
+  rm -r ${TMP_DIR}
 }
 
 fetch_data
-gen_gfwlist_pac
+extract_domains
+gen_gfwlist_config
 dist_release
 clean_up
