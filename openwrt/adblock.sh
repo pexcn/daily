0e2c7c28209db6a7e140bbf78e037a82d22e0547
pexcn <i@pexcn.me>
2018-08-07 21:29:47 +0800

    fix openwrt adblock script url

--- openwrt/adblock.sh
+++ openwrt/adblock.sh
@@ -1,5 +1,5 @@
 #!/bin/ash -e
 
-wget -O /tmp/adblock.conf https://pexcn.me/daily/dnsmasq/adblock.conf --no-check-certificate
+wget -O /tmp/adblock.conf https://pexcn.me/daily/adblock/adblock.conf --no-check-certificate
 mv /tmp/adblock.conf /etc/dnsmasq.d/adblock.conf
 /etc/init.d/dnsmasq restart
