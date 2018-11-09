f3b40a410757a6783e8b5419d005ab5918218729
pexcn <i@pexcn.me>
2018-11-09 15:48:06 +0800

    alexa.sh booting

--- generate.sh
+++ generate.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -e
 
+# adblock
 bash scripts/adblock/adblock.sh
 
+# chnroute
 bash scripts/chnroute/chnroute.sh
 
+# alexa china top list
+bash scripts/alexa/alexa.sh
+
+# gfw domain list
 bash scripts/gfwlist/gfwlist.sh
 
+# china domain list
 bash scripts/chinalist/chinalist.sh
 
+# pac
 bash scripts/pac/gfwlist.sh
 bash scripts/pac/whitelist.sh
 
+# shadowrocket
 bash scripts/shadowrocket/gfwlist.sh
 bash scripts/shadowrocket/tinylist.sh
 bash scripts/shadowrocket/whitelist.sh
