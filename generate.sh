1d6de214c449433c8d6704761721c7ca4270fd1c
pexcn <i@pexcn.me>
2018-11-09 13:55:09 +0800

    gfwlist.sh booting

--- generate.sh
+++ generate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+bash scripts/adblock/adblock.sh
+
 bash scripts/chnroute/chnroute.sh
 
-bash scripts/adblock/adblock.sh
+bash scripts/gfwlist/gfwlist.sh
 
 bash scripts/pac/gfwlist.sh
 bash scripts/pac/whitelist.sh
