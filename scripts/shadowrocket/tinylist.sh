9e461381067dab68ced8f998a08900971e1372e6
pexcn <i@pexcn.me>
2018-11-09 14:45:48 +0800

    tinylist script use my tiny domain list mirror

--- scripts/shadowrocket/tinylist.sh
+++ scripts/shadowrocket/tinylist.sh
@@ -4,55 +4,26 @@ TMP_DIR=`mktemp -d /tmp/shadowrocket.XXXXXX`
 DIST_DIR='dist/shadowrocket'
 DIST_FILE='tinylist.conf'
 
-TINYLIST_URL='https://github.com/gfwlist/tinylist/raw/master/tinylist.txt'
-TINYLIST='tinylist.txt'
-
-TINYLIST_CONTENT='tinylist_content.tmp'
+TINY_DOMAIN_LIST_URL='https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/tinylist.txt'
+TINY_DOMAIN_LIST='tinylist.txt'
 
 function fetch_data() {
   local config_template='template/shadowrocket/tinylist.conf'
-  local extra_template='template/gfwlist_extra_domains.txt'
 
   cp ${config_template} ${TMP_DIR}
-  cp ${extra_template} ${TMP_DIR}
 
   pushd ${TMP_DIR}
-  curl -kLs ${TINYLIST_URL} | base64 -d > ${TINYLIST}
-  popd
-}
-
-function extract_domains() {
-  pushd ${TMP_DIR}
-
-  local tinylist_domains='tinylist_domains.txt'
-  local gfwlist_extra_domains='gfwlist_extra_domains.txt'
-
-  # patterns from @cokebar/gfwlist2dnsmasq
-  local ignore_pattern='^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
-  local head_filter_pattern='s#^(\|\|?)?(https?://)?##g'
-  local tail_filter_pattern='s#/.*$|%2F.*$##g'
-  local domain_pattern='([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)'
-  local wildcard_pattern='s#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*)?#\4#g'
-
-  grep -vE ${ignore_pattern} ${TINYLIST} |
-      sed -r ${head_filter_pattern} |
-      sed -r ${tail_filter_pattern} |
-      grep -E ${domain_pattern} |
-      sed -r ${wildcard_pattern} > ${tinylist_domains}
-  cat ${gfwlist_extra_domains} >> ${tinylist_domains}
-
-  cat ${tinylist_domains} | sort | uniq > ${TINYLIST_CONTENT}
-
+  curl -kLs ${TINY_DOMAIN_LIST_URL} > ${TINY_DOMAIN_LIST}
   popd
 }
 
 function gen_tinylist_config() {
   pushd ${TMP_DIR}
 
-  sed -i 's/^/DOMAIN-SUFFIX,/' ${TINYLIST_CONTENT}
-  sed -i 's/$/,PROXY,force-remote-dns/' ${TINYLIST_CONTENT}
+  sed -i 's/^/DOMAIN-SUFFIX,/' ${TINY_DOMAIN_LIST}
+  sed -i 's/$/,PROXY,force-remote-dns/' ${TINY_DOMAIN_LIST}
 
-  sed -i "s/___TINYLIST_DOMAINS_PLACEHOLDER___/cat ${TINYLIST_CONTENT}/e" ${DIST_FILE}
+  sed -i "s/___TINYLIST_DOMAINS_PLACEHOLDER___/cat ${TINY_DOMAIN_LIST}/e" ${DIST_FILE}
   sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" ${DIST_FILE}
 
   popd
@@ -68,7 +39,6 @@ function clean_up() {
 }
 
 fetch_data
-extract_domains
 gen_tinylist_config
 dist_release
-clean_up
+#clean_up
