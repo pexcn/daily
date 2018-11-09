6152a46c7ba0e2efb64dc34a3b7df0d2540805be
pexcn <i@pexcn.me>
2018-11-09 15:42:32 +0800

    add alexa top list script
    resolve #8

---
+++ scripts/alexa/alexa.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+TMP_DIR=`mktemp -d /tmp/alexa.XXXXXX`
+DIST_DIR='dist/alexa'
+DIST_FILE_TOP_5000='top-cn-5000.txt'
+DIST_FILE_TOP_2000='top-cn-2000.txt'
+DIST_FILE_TOP_500='top-cn-500.txt'
+
+ALEXA_LIST_URL='https://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
+ALEXA_LIST='alexalist.txt'
+CHINA_LIST_URL='https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt'
+CHINA_LIST='chinalist.txt'
+
+function fetch_data() {
+  pushd ${TMP_DIR}
+
+  curl -kLs ${ALEXA_LIST_URL} | gunzip | awk -F ',' '{print $2}' > ${ALEXA_LIST}
+  curl -kLs ${CHINA_LIST_URL} > ${CHINA_LIST}
+
+  popd
+}
+
+function gen_alexa_top_list() {
+  pushd ${TMP_DIR}
+
+  local top_cn='top-cn.txt'
+
+  grep -Fx -f ${CHINA_LIST} ${ALEXA_LIST} > ${top_cn}
+  head -5000 ${top_cn} > ${DIST_FILE_TOP_5000}
+  head -2000 ${top_cn} > ${DIST_FILE_TOP_2000}
+  head -500 ${top_cn} > ${DIST_FILE_TOP_500}
+
+  popd
+}
+
+function dist_release() {
+  mkdir -p ${DIST_DIR}
+  mv ${TMP_DIR}/${DIST_FILE_TOP_5000} ${DIST_DIR}
+  mv ${TMP_DIR}/${DIST_FILE_TOP_2000} ${DIST_DIR}
+  mv ${TMP_DIR}/${DIST_FILE_TOP_500} ${DIST_DIR}
+}
+
+function clean_up() {
+  rm -r ${TMP_DIR}
+}
+
+fetch_data
+gen_alexa_top_list
+dist_release
+clean_up
