c35815caa8c02cddf4af2bec859653bb7dd767ac
pexcn <i@pexcn.me>
2018-12-13 17:43:00 +0800

    adblock: remove full version adblock config

--- scripts/adblock/adblock.sh
+++ scripts/adblock/adblock.sh
@@ -3,24 +3,20 @@
 TMP_DIR=`mktemp -d /tmp/adblock.XXXXXX`
 DIST_DIR='dist/adblock'
 DIST_FILE='adblock.conf'
-DIST_FILE_FULL='adblock_full.conf'
 
 EASYLIST_URL='https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt'
 ABPFX_URL='https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt'
 YOYO_URL='https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext'
-ADAWAY_URL='https://hosts-file.net/ad_servers.txt'
 
 EASY_LIST='easylistchina.txt'
 ABPFX_LIST='abpfx.txt'
 YOYO_LIST='yoyo.txt'
-ADAWAY_LIST='adaway.txt'
 
 function fetch_data() {
   pushd ${TMP_DIR}
   curl -kLs ${EASYLIST_URL} > ${EASY_LIST}
   curl -kLs ${ABPFX_URL} > ${ABPFX_LIST}
   curl -kLs ${YOYO_URL} > ${YOYO_LIST}
-  curl -kLs ${ADAWAY_URL} > ${ADAWAY_LIST}
   popd
 }
 
@@ -30,24 +26,20 @@ function gen_adblock_rules() {
   local easylist_content='easylistchina_content.tmp'
   local abpfx_content='abpfx_content.tmp'
   local yoyo_content='yoyo_content.tmp'
-  local adaway_content='adaway_content.tmp'
 
   cat ${EASY_LIST} | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > ${easylist_content}
   cat ${ABPFX_LIST} | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > ${abpfx_content}
   cat ${YOYO_LIST} | grep -E '^127.0.0.1' | awk '{printf "address=/%s/127.0.0.1\n",$2}' > ${yoyo_content}
-  cat ${ADAWAY_LIST} | sed  $'s/\r$//' | grep -E '^127.0.0.1' | awk '{printf "address=/%s/127.0.0.1\n",$2}' > ${adaway_content}
 
   cat ${easylist_content} ${abpfx_content} ${yoyo_content} | sort | uniq > ${DIST_FILE}
-  cat ${easylist_content} ${abpfx_content} ${yoyo_content} ${adaway_content} | sort | uniq > ${DIST_FILE_FULL}
 
-  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" ${DIST_FILE} ${DIST_FILE_FULL}
+  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" ${DIST_FILE}
   popd
 }
 
 function dist_release() {
   mkdir -p ${DIST_DIR}
   mv ${TMP_DIR}/${DIST_FILE} ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE_FULL} ${DIST_DIR}
 }
 
 function clean_up() {
