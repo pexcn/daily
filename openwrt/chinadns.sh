bea568e673044566c301fdd712276974bf083eb3
pexcn <i@pexcn.me>
2018-12-14 10:21:36 +0800

    openwrt: wget check certificate

--- openwrt/chinadns.sh
+++ openwrt/chinadns.sh
@@ -1,5 +1,5 @@
 #!/bin/ash -e
 
-wget -O /tmp/chnroute.txt https://pexcn.me/daily/chnroute/chnroute.txt --no-check-certificate
+wget -O /tmp/chnroute.txt https://pexcn.me/daily/chnroute/chnroute.txt
 mv /tmp/chnroute.txt /etc/chinadns_chnroute.txt
 /etc/init.d/chinadns restart
