63464a4d4de1b20fda923c374575eea7efb2a780
pexcn <i@pexcn.me>
2019-06-05 22:28:13 +0800

    scripts: refine chnroute

--- scripts/chnroute/chnroute.sh
+++ scripts/chnroute/chnroute.sh
@@ -1,59 +1,61 @@
 #!/bin/bash -e
 
-TMP_DIR=`mktemp -d /tmp/chnroute.XXXXXX`
-DIST_DIR='dist/chnroute'
-DIST_FILE_IPV4='chnroute.txt'
-DIST_FILE_IPV6='chnroute-v6.txt'
+CUR_DIR=$(pwd)
+TMP_DIR=$(mktemp -d /tmp/chnroute.XXXXXX)
+DIST_DIR="$CUR_DIR/dist/chnroute"
+DIST_FILE_IPV4="chnroute.txt"
+DIST_FILE_IPV6="chnroute-v6.txt"
 
-APNIC_URL='https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
-IPIP_URL='https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt'
-
-APNIC_LIST='apnic.txt'
-IPIP_LIST='ipip.txt'
+APNIC_URL="https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+IPIP_URL="https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt"
+APNIC_LIST="apnic.txt"
+IPIP_LIST="ipip.txt"
 
 function fetch_data() {
-  pushd ${TMP_DIR}
-  curl -kLs ${APNIC_URL} > ${APNIC_LIST}
-  curl -kLs ${IPIP_URL} > ${IPIP_LIST}
-  popd
+  cd $TMP_DIR
+
+  curl -sSL $APNIC_URL > $APNIC_LIST
+  curl -sSL $IPIP_URL > $IPIP_LIST
+
+  cd $CUR_DIR
 }
 
 function gen_ipv4_chnroute() {
-  pushd ${TMP_DIR}
+  cd $TMP_DIR
 
-  local apnic_v4='apnic_v4.tmp'
-  local ipip_v4='ipip_v4.tmp'
-  local chnroute_v4='chnroute_v4.tmp'
+  local apnic_v4="apnic_v4.tmp"
+  local ipip_v4="ipip_v4.tmp"
+  local chnroute_v4="chnroute_v4.tmp"
 
-  cat ${APNIC_LIST} | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ${apnic_v4}
-  cat ${IPIP_LIST} > ${ipip_v4}
-  cat ${apnic_v4} ${ipip_v4} | aggregate -q > ${chnroute_v4}
+  cat $APNIC_LIST | grep ipv4 | grep CN | awk -F\| "{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }" > $apnic_v4
+  cat $IPIP_LIST > $ipip_v4
+  cat $apnic_v4 $ipip_v4 | aggregate -q > $chnroute_v4
 
-  mv ${chnroute_v4} ${DIST_FILE_IPV4}
+  mv $chnroute_v4 $DIST_FILE_IPV4
 
-  popd
+  cd $CUR_DIR
 }
 
 function gen_ipv6_chnroute() {
-  pushd ${TMP_DIR}
+  cd $TMP_DIR
 
-  local apnic_v6='apnic_v6.tmp'
+  local apnic_v6="apnic_v6.tmp"
 
-  cat ${APNIC_LIST} | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ${apnic_v6}
+  cat $APNIC_LIST | grep ipv6 | grep CN | awk -F\| "{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }" > ${apnic_v6}
 
-  mv ${apnic_v6} ${DIST_FILE_IPV6}
+  mv $apnic_v6 $DIST_FILE_IPV6
 
-  popd
+  cd $CUR_DIR
 }
 
 function dist_release() {
-  mkdir -p ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE_IPV4} ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE_IPV6} ${DIST_DIR}
+  mkdir -p $DIST_DIR
+  cp $TMP_DIR/$DIST_FILE_IPV4 $DIST_DIR
+  cp $TMP_DIR/$DIST_FILE_IPV6 $DIST_DIR
 }
 
 function clean_up() {
-  rm -r ${TMP_DIR}
+  rm -r $TMP_DIR
 }
 
 fetch_data
