256cd223622cc53e7d15880a40faa59a2bf36608
pexcn <i@pexcn.me>
2019-06-05 22:18:46 +0800

    scripts: refine adblock list

--- scripts/adblock/adblock.sh
+++ scripts/adblock/adblock.sh
@@ -1,37 +1,39 @@
 #!/bin/bash -e
 
-TMP_DIR=`mktemp -d /tmp/adblock.XXXXXX`
-DIST_DIR='dist/adblock'
-DIST_FILE='adblock.conf'
+CUR_DIR=$(pwd)
+TMP_DIR=$(mktemp -d /tmp/adblock.XXXXXX)
+DIST_DIR="$CUR_DIR/dist/adblock"
+DIST_FILE="adblock.conf"
 
-ADLIST_URL='https://raw.githubusercontent.com/pexcn/daily/gh-pages/adlist/adlist.txt'
-ADLIST='adlist.txt'
+ADLIST="$CUR_DIR/dist/adlist/adlist.txt"
 
 function fetch_data() {
-  pushd ${TMP_DIR}
-  curl -kLs ${ADLIST_URL} > ${ADLIST}
-  popd
+  cd $TMP_DIR
+
+  cp $ADLIST .
+
+  cd $CUR_DIR
 }
 
-function gen_adblock_rules() {
-  pushd ${TMP_DIR}
+function gen_adblock_list() {
+  cd $TMP_DIR
 
-  sed -e 's/^/address=\//' -e 's/$/\/127.0.0.1/' ${ADLIST} > ${DIST_FILE}
-  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" ${DIST_FILE}
+  sed -e "s/^/address=\//" -e "s/$/\/127.0.0.1/" $ADLIST > $DIST_FILE
+  sed -i "1i#\n# Update: $(date +'%Y-%m-%d %T')\n#\n" $DIST_FILE
 
-  popd
+  cd $CUR_DIR
 }
 
 function dist_release() {
-  mkdir -p ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE} ${DIST_DIR}
+  mkdir -p $DIST_DIR
+  cp $TMP_DIR/$DIST_FILE $DIST_DIR
 }
 
 function clean_up() {
-  rm -r ${TMP_DIR}
+  rm -r $TMP_DIR
 }
 
 fetch_data
-gen_adblock_rules
+gen_adblock_list
 dist_release
 clean_up
