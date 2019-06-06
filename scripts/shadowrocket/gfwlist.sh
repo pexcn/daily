693330a10b6e924aa67e434ba9c9fdc5d6e99243
pexcn <i@pexcn.me>
2019-06-06 13:59:43 +0800

    scripts: refine shadowrocket gfw list

--- scripts/shadowrocket/gfwlist.sh
+++ scripts/shadowrocket/gfwlist.sh
@@ -1,41 +1,43 @@
 #!/bin/bash -e
 
-TMP_DIR=`mktemp -d /tmp/shadowrocket.XXXXXX`
-DIST_DIR='dist/shadowrocket'
-DIST_FILE='gfwlist.conf'
+CUR_DIR=$(pwd)
+TMP_DIR=$(mktemp -d /tmp/shadowrocket.XXXXXX)
+DIST_DIR="$CUR_DIR/dist/shadowrocket"
+DIST_FILE="gfwlist.conf"
 
-GFW_DOMAIN_LIST_URL='https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt'
-GFW_DOMAIN_LIST='gfwlist.txt'
+GFW_DOMAIN_LIST_SRC="$CUR_DIR/dist/gfwlist/gfwlist.txt"
+GFW_DOMAIN_LIST=$(basename $GFW_DOMAIN_LIST_SRC)
 
 function fetch_data() {
-  local config_template='template/shadowrocket/gfwlist.conf'
+  cd $TMP_DIR
 
-  cp ${config_template} ${TMP_DIR}
+  local config_template="$CUR_DIR/template/shadowrocket/gfwlist.conf"
 
-  pushd ${TMP_DIR}
-  curl -kLs ${GFW_DOMAIN_LIST_URL} > ${GFW_DOMAIN_LIST}
-  popd
+  cp $config_template .
+  cp $GFW_DOMAIN_LIST_SRC $GFW_DOMAIN_LIST 
+
+  cd $CUR_DIR
 }
 
 function gen_gfwlist_config() {
-  pushd ${TMP_DIR}
+  cd $TMP_DIR
 
-  sed -i 's/^/DOMAIN-SUFFIX,/' ${GFW_DOMAIN_LIST}
-  sed -i 's/$/,PROXY,force-remote-dns/' ${GFW_DOMAIN_LIST}
+  sed -i 's/^/DOMAIN-SUFFIX,/' $GFW_DOMAIN_LIST
+  sed -i 's/$/,PROXY,force-remote-dns/' $GFW_DOMAIN_LIST
 
-  sed -i "s/___GFWLIST_DOMAINS_PLACEHOLDER___/cat ${GFW_DOMAIN_LIST}/e" ${DIST_FILE}
-  sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" ${DIST_FILE}
+  sed -i "s/___GFWLIST_DOMAINS_PLACEHOLDER___/cat $GFW_DOMAIN_LIST/e" $DIST_FILE
+  sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" $DIST_FILE
 
-  popd
+  cd $CUR_DIR
 }
 
 function dist_release() {
-  mkdir -p ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE} ${DIST_DIR}
+  mkdir -p $DIST_DIR
+  cp $TMP_DIR/$DIST_FILE $DIST_DIR
 }
 
 function clean_up() {
-  rm -r ${TMP_DIR}
+  rm -r $TMP_DIR
 }
 
 fetch_data
