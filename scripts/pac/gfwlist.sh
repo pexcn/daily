5ac21dc014b198719a99ceaa4edd8a72a80b28d4
pexcn <i@pexcn.me>
2019-06-06 13:44:31 +0800

    scripts: refine pac gfw list

--- scripts/pac/gfwlist.sh
+++ scripts/pac/gfwlist.sh
@@ -1,42 +1,44 @@
 #!/bin/bash -e
 
-TMP_DIR=`mktemp -d /tmp/pac.XXXXXX`
-DIST_DIR='dist/pac'
-DIST_FILE='gfwlist.pac'
+CUR_DIR=$(pwd)
+TMP_DIR=$(mktemp -d /tmp/pac.XXXXXX)
+DIST_DIR="$CUR_DIR/dist/pac"
+DIST_FILE="gfwlist.pac"
 
-GFW_DOMAIN_LIST_URL='https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt'
-GFW_DOMAIN_LIST='gfwlist.txt'
+GFW_DOMAIN_LIST_SRC="$CUR_DIR/dist/gfwlist/gfwlist.txt"
+GFW_DOMAIN_LIST=$(basename $GFW_DOMAIN_LIST_SRC)
 
 function fetch_data() {
-  local pac_template='template/pac/gfwlist.pac'
+  cd $TMP_DIR
 
-  cp ${pac_template} ${TMP_DIR}
+  local pac_template="$CUR_DIR/template/pac/gfwlist.pac"
 
-  pushd ${TMP_DIR}
-  curl -kLs ${GFW_DOMAIN_LIST_URL} > ${GFW_DOMAIN_LIST}
-  popd
+  cp $pac_template .
+  cp $GFW_DOMAIN_LIST_SRC $GFW_DOMAIN_LIST 
+
+  cd $CUR_DIR
 }
 
 function gen_gfwlist_pac() {
-  pushd ${TMP_DIR}
+  cd $TMP_DIR
 
-  sed -i 's/^/  "/' ${GFW_DOMAIN_LIST}
-  sed -i 's/$/": 1,/' ${GFW_DOMAIN_LIST}
-  sed -i '$ s/": 1,/": 1/g' ${GFW_DOMAIN_LIST}
+  sed -i 's/^/  "/' $GFW_DOMAIN_LIST
+  sed -i 's/$/": 1,/' $GFW_DOMAIN_LIST
+  sed -i '$ s/": 1,/": 1/g' $GFW_DOMAIN_LIST
 
-  sed -i "s/___GFWLIST_DOMAINS_PLACEHOLDER___/cat ${GFW_DOMAIN_LIST}/e" ${DIST_FILE}
-  sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" ${DIST_FILE}
+  sed -i "s/___GFWLIST_DOMAINS_PLACEHOLDER___/cat $GFW_DOMAIN_LIST/e" $DIST_FILE
+  sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" $DIST_FILE
 
-  popd
+  cd $CUR_DIR
 }
 
 function dist_release() {
-  mkdir -p ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE} ${DIST_DIR}
+  mkdir -p $DIST_DIR
+  cp $TMP_DIR/$DIST_FILE $DIST_DIR
 }
 
 function clean_up() {
-  rm -r ${TMP_DIR}
+  rm -r $TMP_DIR
 }
 
 fetch_data
