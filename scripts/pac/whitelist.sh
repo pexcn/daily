ff2fd8671193ff0c37d74023d0dc43559a82101e
pexcn <i@pexcn.me>
2019-06-06 13:53:05 +0800

    scripts: refine pac white list

--- scripts/pac/whitelist.sh
+++ scripts/pac/whitelist.sh
@@ -1,43 +1,45 @@
 #!/bin/bash -e
 
-TMP_DIR=`mktemp -d /tmp/pac.XXXXXX`
-DIST_DIR='dist/pac'
-DIST_FILE='whitelist.pac'
+CUR_DIR=$(pwd)
+TMP_DIR=$(mktemp -d /tmp/pac.XXXXXX)
+DIST_DIR="$CUR_DIR/dist/pac"
+DIST_FILE="whitelist.pac"
 
-CHINA_LIST_URL='https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt'
-CHINA_LIST='chinalist.tmp'
+CHINA_LIST_SRC="$CUR_DIR/dist/chinalist/chinalist.txt"
+CHINA_LIST=$(basename $CHINA_LIST_SRC)
 
 function fetch_data() {
-  local pac_template='template/pac/whitelist.pac'
+  cd $TMP_DIR
 
-  cp ${pac_template} ${TMP_DIR}
+  local pac_template="$CUR_DIR/template/pac/whitelist.pac"
 
-  pushd ${TMP_DIR}
-  curl -kLs ${CHINA_LIST_URL} > ${CHINA_LIST}
-  popd
+  cp $pac_template .
+  cp $CHINA_LIST_SRC $CHINA_LIST
+
+  cd $CUR_DIR
 }
 
 function gen_whitelist_pac() {
-  pushd ${TMP_DIR}
+  cd $TMP_DIR
 
-  sed -i 's/^/  "/' ${CHINA_LIST}
-  sed -i 's/$/": 1,/' ${CHINA_LIST}
+  sed -i 's/^/  "/' $CHINA_LIST
+  sed -i 's/$/": 1,/' $CHINA_LIST
 
-  sed -i '$ s/.$//g' ${CHINA_LIST}
+  sed -i '$ s/.$//g' $CHINA_LIST
 
-  sed -i "s/___CHINA_DOMAINS_PLACEHOLDER___/cat ${CHINA_LIST}/e" ${DIST_FILE}
-  sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" ${DIST_FILE}
+  sed -i "s/___CHINA_DOMAINS_PLACEHOLDER___/cat $CHINA_LIST/e" $DIST_FILE
+  sed -i "s/___UPDATE_TIME_PLACEHOLDER___/$(date +'%Y-%m-%d %T')/g" $DIST_FILE
 
-  popd
+  cd $CUR_DIR
 }
 
 function dist_release() {
-  mkdir -p ${DIST_DIR}
-  mv ${TMP_DIR}/${DIST_FILE} ${DIST_DIR}
+  mkdir -p $DIST_DIR
+  cp $TMP_DIR/$DIST_FILE $DIST_DIR
 }
 
 function clean_up() {
-  rm -r ${TMP_DIR}
+  rm -r $TMP_DIR
 }
 
 fetch_data
