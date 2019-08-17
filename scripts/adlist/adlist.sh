2dcd48faf984cfb7b6d2d839ebec3ae57398dee6
pexcn <i@pexcn.me>
2019-08-17 16:54:35 +0800

    adlist: add upstream from disconnect

--- scripts/adlist/adlist.sh
+++ scripts/adlist/adlist.sh
@@ -8,9 +8,11 @@ DIST_FILE="adlist.txt"
 ADAWAY_URL="https://adaway.org/hosts.txt"
 EASYLIST_URL="https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt"
 YOYO_URL="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
+DISCONNECT_URL="https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
 ADAWAY_LIST="adaway.txt"
-EASY_LIST="easylistchina.txt"
+EASY_LIST="easylist.txt"
 YOYO_LIST="yoyo.txt"
+DISCONNECT_LIST="disconnect.txt"
 
 TOPLIST_SRC="$CUR_DIR/dist/toplist/toplist.txt"
 TOPLIST=$(basename $TOPLIST_SRC)
@@ -20,7 +22,8 @@ function fetch_data() {
 
   curl -sSL $ADAWAY_URL > $ADAWAY_LIST
   curl -sSL $EASYLIST_URL > $EASY_LIST
-  curl -ksSL $YOYO_URL > $YOYO_LIST
+  curl -sSL -k $YOYO_URL > $YOYO_LIST
+  curl -sSL $DISCONNECT_URL > $DISCONNECT_LIST
   cp $TOPLIST_SRC $TOPLIST
 
   cd $CUR_DIR
@@ -32,6 +35,7 @@ function gen_adlist() {
   local adaway_content="adaway_content.tmp"
   local easylist_content="easylist_content.tmp"
   local yoyo_content="yoyo_content.tmp"
+  local disconnect_content="disconnect_content.tmp"
 
   local adlist_tmp="adlist.tmp"
   local adlist_part_1="adlist_part_1.tmp"
@@ -40,8 +44,9 @@ function gen_adlist() {
   cat $ADAWAY_LIST | sed $"s/\r$//" | sed '/^$/d' | grep -v "^#"  | grep -v "localhost" | cut -d ' ' -f 2 > $adaway_content
   cat $EASY_LIST | grep ^\|\|[^\*]*\^$ | sed -e "s/||//" -e "s/\^//" > $easylist_content
   cat $YOYO_LIST > $yoyo_content
+  cat $DISCONNECT_LIST | sed '1,3d' | sed '/^$/d' | grep -v "#" > $disconnect_content
 
-  cat $adaway_content $easylist_content $yoyo_content | sort -u > $adlist_tmp
+  cat $adaway_content $easylist_content $yoyo_content $disconnect_content | sort -u > $adlist_tmp
 
   # sort by toplist
   grep -Fx -f $adlist_tmp $TOPLIST > $adlist_part_1
