c4828b697d6c99840739e529ab6cb3d532a7991a
pexcn <i@pexcn.me>
2019-08-18 12:16:23 +0800

    adlist: refine #28

--- scripts/adlist/adlist.sh
+++ scripts/adlist/adlist.sh
@@ -2,29 +2,24 @@
 
 CUR_DIR=$(pwd)
 TMP_DIR=$(mktemp -d /tmp/adlist.XXXXXX)
-DIST_DIR="$CUR_DIR/dist/adlist"
-DIST_FILE="adlist.txt"
+
+DIST_FILE="dist/adlist/adlist.txt"
+DIST_DIR="$(dirname $DIST_FILE)"
+DIST_NAME="$(basename $DIST_FILE)"
 
 ADAWAY_URL="https://adaway.org/hosts.txt"
 EASYLIST_URL="https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt"
 YOYO_URL="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
 DISCONNECT_URL="https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
-ADAWAY_LIST="adaway.txt"
-EASY_LIST="easylist.txt"
-YOYO_LIST="yoyo.txt"
-DISCONNECT_LIST="disconnect.txt"
-
-TOPLIST_SRC="$CUR_DIR/dist/toplist/toplist.txt"
-TOPLIST=$(basename $TOPLIST_SRC)
 
 function fetch_data() {
   cd $TMP_DIR
 
-  curl -sSL -k $ADAWAY_URL > $ADAWAY_LIST
-  curl -sSL -k $EASYLIST_URL > $EASY_LIST
-  curl -sSL -k $YOYO_URL > $YOYO_LIST
-  curl -sSL -k $DISCONNECT_URL > $DISCONNECT_LIST
-  cp $TOPLIST_SRC $TOPLIST
+  curl -sSL -k --connect-timeout 10 -o adaway.txt $ADAWAY_URL
+  curl -sSL -k --connect-timeout 10 -o easylist.txt $EASYLIST_URL
+  curl -sSL -k --connect-timeout 10 -o yoyo.txt $YOYO_URL
+  curl -sSL -k --connect-timeout 10 -o disconnect.txt $DISCONNECT_URL
+  cp $CUR_DIR/dist/toplist/toplist.txt .
 
   cd $CUR_DIR
 }
@@ -32,33 +27,62 @@ function fetch_data() {
 function gen_adlist() {
   cd $TMP_DIR
 
-  local adaway_content="adaway_content.tmp"
-  local easylist_content="easylist_content.tmp"
-  local yoyo_content="yoyo_content.tmp"
-  local disconnect_content="disconnect_content.tmp"
+  local adaway_tmp="adaway.tmp"
+  local easylist_tmp="easylist.tmp"
+  local yoyo_tmp="yoyo.tmp"
+  local disconnect_tmp="disconnect.tmp"
 
   local adlist_tmp="adlist.tmp"
   local adlist_part_1="adlist_part_1.tmp"
   local adlist_part_2="adlist_part_2.tmp"
 
-  cat $ADAWAY_LIST | sed $'s/\r$//' | sed '/^$/d' | grep -v "^#"  | grep -v "localhost" | cut -d ' ' -f 2 > $adaway_content
-  cat $EASY_LIST | grep ^\|\|[^\*]*\^$ | sed -e 's/||//' -e 's/\^//' > $easylist_content
-  cat $YOYO_LIST > $yoyo_content
-  cat $DISCONNECT_LIST | sed '1,3d' | sed '/^$/d' | grep -v "#" > $disconnect_content
+  # adaway
+  cat adaway.txt |
+    # convert to unix format
+    sed $'s/\r$//' |
+    # ignore empty lines
+    sed '/^$/d' |
+    # ignore comment lines
+    grep -v "^#" |
+    # ignore `localhost`
+    grep -v "localhost" |
+    cut -d ' ' -f 2 > $adaway_tmp
 
-  cat $adaway_content $easylist_content $yoyo_content $disconnect_content | sort -u > $adlist_tmp
+  # easylist
+  cat easylist.txt |
+    # extract domains
+    grep ^\|\|[^\*]*\^$ |
+    # clean domains
+    sed -e 's/||//' -e 's/\^//' > $easylist_tmp
 
+  # yoyo
+  cat yoyo.txt > $yoyo_tmp
+
+  # disconnect
+  cat disconnect.txt |
+    # ignore 1-3 lines
+    sed '1,3d' |
+    # ignore empty lines
+    sed '/^$/d' |
+    # ignore comment lines
+    grep -v "#" > $disconnect_tmp
+
+  # merge all list and sort unique
+  cat $adaway_tmp $easylist_tmp $yoyo_tmp $disconnect_tmp | sort -u > $adlist_tmp
+
+  # find intersection set
+  grep -Fx -f $adlist_tmp toplist.txt > $adlist_part_1
+  # find difference set
+  sort $adlist_tmp toplist.txt toplist.txt | uniq -u > $adlist_part_2
   # sort by toplist
-  grep -Fx -f $adlist_tmp $TOPLIST > $adlist_part_1
-  sort $adlist_tmp $TOPLIST $TOPLIST | uniq -u > $adlist_part_2
-  cat $adlist_part_1 $adlist_part_2 > $DIST_FILE
+  cat $adlist_part_1 $adlist_part_2 > $DIST_NAME
 
   cd $CUR_DIR
 }
 
 function dist_release() {
   mkdir -p $DIST_DIR
-  cp $TMP_DIR/$DIST_FILE $DIST_DIR
+  cp $TMP_DIR/$DIST_NAME $DIST_FILE
 }
 
 function clean_up() {
