ca5cf2dad82e39f0a19b94265d42682393f62337
pexcn <i@pexcn.me>
2019-09-01 21:32:41 +0800

    openwrt: disallow cleandns script restart shadowsocks

--- openwrt/cleandns.sh
+++ openwrt/cleandns.sh
@@ -8,4 +8,3 @@ wget $TARGET_URL -O $TEMP_FILE -T 10 --no-cookies --no-hsts
 mkdir -p $(dirname $TARGET_PATH) && mv -f $TEMP_FILE $TARGET_PATH
 
 /etc/init.d/cleandns restart
-/etc/init.d/shadowsocks restart
