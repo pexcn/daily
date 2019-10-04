19cd6332f144215952b83660ae454d040098215e
pexcn <i@pexcn.me>
2019-10-04 23:56:23 +0800

    openwrt: chinadns-ng supported gfwlist

--- openwrt/chinadns-ng.sh
+++ openwrt/chinadns-ng.sh
@@ -4,12 +4,16 @@ TARGET_URL_1="https://pexcn.me/daily/chnroute/chnroute.txt"
 TARGET_PATH_1="/etc/chinadns-ng/chnroute.txt"
 TARGET_URL_2="https://pexcn.me/daily/chnroute/chnroute-v6.txt"
 TARGET_PATH_2="/etc/chinadns-ng/chnroute6.txt"
+TARGET_URL_3="https://pexcn.me/daily/gfwlist/gfwlist.txt"
+TARGET_PATH_3="/etc/chinadns-ng/gfwlist.txt"
 TEMP_FILE="/tmp/$(date +%s).tmp"
 
 wget $TARGET_URL_1 -O $TEMP_FILE -T 10 --no-cookies --no-hsts
 mkdir -p $(dirname $TARGET_PATH_1) && mv -f $TEMP_FILE $TARGET_PATH_1
 wget $TARGET_URL_2 -O $TEMP_FILE -T 10 --no-cookies --no-hsts
 mkdir -p $(dirname $TARGET_PATH_2) && mv -f $TEMP_FILE $TARGET_PATH_2
+wget $TARGET_URL_3 -O $TEMP_FILE -T 10 --no-cookies --no-hsts
+mkdir -p $(dirname $TARGET_PATH_3) && mv -f $TEMP_FILE $TARGET_PATH_3
 
 /etc/init.d/chinadns-ng restart
 /etc/init.d/shadowsocks restart
