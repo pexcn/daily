1300161393670455719c1412d1e8eceb2f1a66c5
pexcn <i@pexcn.me>
2020-05-24 14:39:33 +0800

    adblock: rewrite script
    ref #42

--- scripts/adblock/adblock.sh
+++ scripts/adblock/adblock.sh
@@ -4,51 +4,46 @@ set -o pipefail
 CUR_DIR=$(pwd)
 TMP_DIR=$(mktemp -d /tmp/adblock.XXXXXX)
 
-DIST_FILE="dist/adblock/adblock.conf"
-DIST_DIR="$(dirname $DIST_FILE)"
-DIST_NAME="$(basename $DIST_FILE)"
+SRC_FILE="$CUR_DIR/dist/adlist/adlist.txt"
+DEST_FILE="dist/adblock/adblock.conf"
 
-fetch_data() {
+fetch_src() {
   cd $TMP_DIR
 
-  cp $CUR_DIR/dist/adlist/adlist.txt .
+  cp $SRC_FILE .
 
   cd $CUR_DIR
 }
 
-gen_adblock_list() {
+gen_list() {
   cd $TMP_DIR
 
   local ipv4_regex="[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
 
-  # date
-  cat <<- EOF > $DIST_NAME
-	#
-	# Update: $(date +'%Y-%m-%d %T')
-	#
+  # ignore ipv4 address
+  grep -v "$ipv4_regex" adlist.txt > adblock.conf
+  # convert to dnsmasq format
+  sed -i -e 's/^/server=\//' -e 's/$/\//' adblock.conf
 
-	EOF
-
-  cat adlist.txt |
-    # convert to dnsmasq format
-    sed -e "s/^/server=\//" -e "s/$/\//" |
-	# ignore ipv4 address
-    grep -v "$ipv4_regex" >> $DIST_NAME
+  # append date tag
+  sed -i '1i#' adblock.conf
+  sed -i "2i# Update: $(date +'%Y-%m-%d %T')" adblock.conf
+  sed -i '3i#' adblock.conf
+  sed -i '4i\\' adblock.conf
 
   cd $CUR_DIR
 }
 
-dist_release() {
-  mkdir -p $DIST_DIR
-  cp $TMP_DIR/$DIST_NAME $DIST_FILE
+copy_dest() {
+  install -D $TMP_DIR/adblock.conf $DEST_FILE
 }
 
 clean_up() {
   rm -r $TMP_DIR
-  echo "[adblock]: OK."
+  echo "[$(basename $0 .sh)]: done."
 }
 
-fetch_data
-gen_adblock_list
-dist_release
+fetch_src
+gen_list
+copy_dest
 clean_up
