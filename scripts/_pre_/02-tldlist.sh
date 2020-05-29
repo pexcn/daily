8923b42f639aba9afa3d34ab1f02432cb754dd10
pexcn <i@pexcn.me>
2020-05-29 14:56:15 +0800

    tldlist: add missing tlds
    ref: #44

--- scripts/_pre_/02-tldlist.sh
+++ scripts/_pre_/02-tldlist.sh
@@ -6,6 +6,7 @@ TMP_DIR=$(mktemp -d /tmp/tldlist.XXXXXX)
 
 SRC_URL_1="https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
 SRC_URL_2="https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat"
+SRC_FILE="$CUR_DIR/template/tld-extras.txt"
 DEST_FILE="dist/tldlist/tldlist.txt"
 
 gen_list() {
@@ -15,7 +16,7 @@ gen_list() {
   curl -sSL $SRC_URL_1 |
     # remove comment lines
     sed '/^#/ d' |
-    # remove empty lines
+    # uppercase to lowercase
     tr '[:upper:]' '[:lower:]' > iana-tlds.txt
 
   # publicsuffix
@@ -33,8 +34,15 @@ gen_list() {
     # limit domain length
     awk -F '.' 'length($1) >= 2 && length($1) <= 3 && length($2) <= 3' > publicsuffix-tlds.txt
 
+  # extras
+  cat $SRC_FILE |
+    # remove comment lines
+    sed '/^#/ d' |
+    # remove empty lines
+    sed '/^$/d' > tld-extras.txt
+
   # merge to tldlist
-  cat iana-tlds.txt publicsuffix-tlds.txt | awk '!x[$0]++' > tldlist.txt
+  cat iana-tlds.txt publicsuffix-tlds.txt tld-extras.txt | awk '!x[$0]++' > tldlist.txt
 
   cd $CUR_DIR
 }
