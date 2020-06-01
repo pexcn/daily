6887e8be34e3a821b223ef78af2a4d11a4191614
pexcn <i@pexcn.me>
2020-06-01 21:46:56 +0800

    chnroute: refine code

--- scripts/chnroute/chnroute.sh
+++ scripts/chnroute/chnroute.sh
@@ -14,8 +14,8 @@ fetch_src() {
   cd $TMP_DIR
 
   curl -sSL $SRC_URL_1 -o apnic.txt
-  curl -sSL $SRC_URL_2 -o ipip.txt
-  curl -sSL $SRC_URL_3 -o ipdeny.txt
+  curl -sSL $SRC_URL_2 -o ipip-v4.txt
+  curl -sSL $SRC_URL_3 -o ipdeny-v6.txt
 
   cd $CUR_DIR
 }
@@ -24,11 +24,12 @@ gen_list_v4() {
   cd $TMP_DIR
 
   # convert to cidr format
-  cat apnic.txt | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > apnic.tmp
+  cat apnic.txt | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > apnic-v4.tmp
   # add newline to end of file only if newline doesn't exist
-  sed '$a\' ipip.txt > ipip.tmp
+  sed '$a\' ipip-v4.txt > ipip-v4.tmp
+
   # ipv4 cidr merge
-  cat apnic.tmp ipip.tmp | $CUR_DIR/tools/ip-dedup/obj/ip-dedup -4 > chnroute.txt
+  cat apnic-v4.tmp ipip-v4.tmp | $CUR_DIR/tools/ip-dedup/obj/ip-dedup -4 > chnroute.txt
 
   cd $CUR_DIR
 }
@@ -37,10 +38,11 @@ gen_list_v6() {
   cd $TMP_DIR
 
   # convert to cidr format
-  cat apnic.txt | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, $5) }' > apnic6.tmp
-  cat ipdeny.txt > ipdeny6.tmp
+  cat apnic.txt | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, $5) }' > apnic-v6.tmp
+  cat ipdeny-v6.txt > ipdeny-v6.tmp
+
   # ipv6 cidr merge
-  cat apnic6.tmp ipdeny6.tmp | $CUR_DIR/tools/ip-dedup/obj/ip-dedup -6 > chnroute-v6.txt
+  cat apnic-v6.tmp ipdeny-v6.tmp | $CUR_DIR/tools/ip-dedup/obj/ip-dedup -6 > chnroute-v6.txt
 
   cd $CUR_DIR
 }
