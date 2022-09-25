f84e7c89d02cb394b195856e0b8263bbf98b98a6
pexcn <i@pexcn.me>
2022-09-25 14:25:21 +0800

    refine openwrt cron job

--- openwrt/chinadns-ng.sh
+++ openwrt/chinadns-ng.sh
@@ -2,14 +2,19 @@
 set -o pipefail
 
 [ -d /etc/chinadns-ng ] || mkdir /etc/chinadns-ng
-wget https://pexcn.me/daily/chnroute/chnroute.txt -O /tmp/chnroute.txt.tmp
-mv -f /tmp/chnroute.txt.tmp /etc/chinadns-ng/chnroute.txt
-wget https://pexcn.me/daily/chnroute/chnroute-v6.txt -O /tmp/chnroute-v6.txt.tmp
-mv -f /tmp/chnroute-v6.txt.tmp /etc/chinadns-ng/chnroute6.txt
-wget https://pexcn.me/daily/gfwlist/gfwlist.txt -O /tmp/gfwlist.txt.tmp
-mv -f /tmp/gfwlist.txt.tmp /etc/chinadns-ng/gfwlist.txt
-wget https://pexcn.me/daily/chinalist/chinalist.txt -O /tmp/chinalist.txt.tmp
-mv -f /tmp/chinalist.txt.tmp /etc/chinadns-ng/chinalist.txt
+
+wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute.txt -O /etc/chinadns-ng/chnroute.tmp
+mv -f /etc/chinadns-ng/chnroute.tmp /etc/chinadns-ng/chnroute.txt
+
+wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute6.txt -O /etc/chinadns-ng/chnroute6.tmp
+mv -f /etc/chinadns-ng/chnroute6.tmp /etc/chinadns-ng/chnroute6.txt
+
+wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt -O /etc/chinadns-ng/gfwlist.tmp
+mv -f /etc/chinadns-ng/gfwlist.tmp /etc/chinadns-ng/gfwlist.txt
+sed -n -i '1,100p' /etc/chinadns-ng/gfwlist.txt
+
+wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -O /etc/chinadns-ng/chinalist.tmp
+mv -f /etc/chinadns-ng/chinalist.tmp /etc/chinadns-ng/chinalist.txt
+sed -n -i '1,100p' /etc/chinadns-ng/chinalist.txt
 
 /etc/init.d/chinadns-ng restart
-/etc/init.d/shadowsocks restart
--- openwrt/chinadns.sh
+++ openwrt/chinadns.sh
@@ -5,4 +5,3 @@ wget https://pexcn.me/daily/chnroute/chnroute.txt -O /tmp/chnroute.txt.tmp
 mv -f /tmp/chnroute.txt.tmp /etc/chinadns_chnroute.txt
 
 /etc/init.d/chinadns restart
-/etc/init.d/shadowsocks restart
