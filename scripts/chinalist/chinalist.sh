ff2f33dd2e836b4ffefe8791844bd476e0abf8d5
pexcn <pexcn97@gmail.com>
2025-11-17 21:55:21 +0800

    chinalist: remove google domains
    I usually use this list in conjunction with the `add_tagchn_ip` option of chinadns-ng,
    but some domains in google.china.conf resolve to blocked ips.

--- scripts/chinalist/chinalist.sh
+++ scripts/chinalist/chinalist.sh
@@ -5,7 +5,6 @@ CUR_DIR=$(pwd)
 TMP_DIR=$(mktemp -d /tmp/chinalist.XXXXXX)
 
 SRC_URL_1="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf"
-SRC_URL_2="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
 SRC_URL_3="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
 SRC_URL_4="https://raw.githubusercontent.com/pexcn/daily-extras/master/chinalist-extras.txt"
 SRC_FILE="$CUR_DIR/dist/toplist/toplist.txt"
@@ -15,7 +14,6 @@ fetch_src() {
   cd $TMP_DIR
 
   curl -sSL $SRC_URL_1 -o apple.conf
-  curl -sSL $SRC_URL_2 -o google.conf
   curl -sSL $SRC_URL_3 -o china.conf
   curl -sSL $SRC_URL_4 -o chinalist-extras.txt
   cp $SRC_FILE .
@@ -26,7 +24,7 @@ fetch_src() {
 gen_list() {
   cd $TMP_DIR
 
-  cat apple.conf google.conf china.conf |
+  cat apple.conf china.conf |
     # remove empty lines
     sed '/^[[:space:]]*$/d' |
     # remove comment lines
